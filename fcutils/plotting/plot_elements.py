dcf647ba58def02197e583b8122def94e3d20308
FedeClaudi <federicoclaudi@protonmail.com>
2020-01-26 12:30:52 +0000

    update

--- fcutils/plotting/plot_elements.py
+++ fcutils/plotting/plot_elements.py
@@ -82,12 +82,12 @@ def plot_shaded_withline(ax, x, y, z=None, label=None, alpha=.15,  **kwargs):
 		alpha {float} -- [description] (default: {.15})
 	"""
 	if z is not None:
-		# ax.fill_between(x, z, y, alpha=alpha, **kwargs)
-		ax.fill_betweenx(y, z, x, alpha=alpha, **kwargs)
+		ax.fill_between(x, z, y, alpha=alpha, **kwargs)
+		# ax.fill_betweenx(y, z, x, alpha=alpha, **kwargs)
 
 	else:
-		# ax.fill_between(x, y, alpha=alpha, **kwargs)
-		ax.fill_betweenx(y, x, alpha=alpha, **kwargs)
+		ax.fill_between(x, y, alpha=alpha, **kwargs)
+		# ax.fill_betweenx(y, x, alpha=alpha, **kwargs)
 
 	ax.plot(x, y, alpha=1, label=label, **kwargs)
 
