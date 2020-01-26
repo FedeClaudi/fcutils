46af19a3aa02eadaa404babef99fc051ce766eff
FedeClaudi <federicoclaudi@protonmail.com>
2020-01-26 11:37:21 +0000

    bump

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="fcutils",
-    version="0.1",
+    version="0.1.1",
     author_email="federicoclaudi@protonmail.com",
     description="bunch of utility functions",
+    packages=find_namespace_packages(exclude=()),
+    include_package_data=True,
+    url="https://github.com/FedeClaudi/fcutils",
+    author="Federico Claudi",
+    zip_safe=False,
 )
