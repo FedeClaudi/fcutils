2d8f717e7e08a4d67f0b491c3fc7099a86893904
FedeClaudi <federicoclaudi@protonmail.com>
2020-01-28 21:39:56 +0000

    bump

--- fcutils/maths/utils.py
+++ fcutils/maths/utils.py
@@ -20,14 +20,14 @@ def binArray(data, axis, binstep, binsize, func=np.nanmean):
 	"""
 		Bins a numpy array
 	"""
-    data = np.array(data)
-    dims = np.array(data.shape)
-    argdims = np.arange(data.ndim)
-    argdims[0], argdims[axis]= argdims[axis], argdims[0]
-    data = data.transpose(argdims)
-    data = [func(np.take(data,np.arange(int(i*binstep),int(i*binstep+binsize)),0),0) for i in np.arange(dims[axis]//binstep)]
-    data = np.array(data).transpose(argdims)
-    return data
+	data = np.array(data)
+	dims = np.array(data.shape)
+	argdims = np.arange(data.ndim)
+	argdims[0], argdims[axis]= argdims[axis], argdims[0]
+	data = data.transpose(argdims)
+	data = [func(np.take(data,np.arange(int(i*binstep),int(i*binstep+binsize)),0),0) for i in np.arange(dims[axis]//binstep)]
+	data = np.array(data).transpose(argdims)
+	return data
 
 def log_transform(im):
 	'''returns log(image) scaled to the interval [0,1]'''
