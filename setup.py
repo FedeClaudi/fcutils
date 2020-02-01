bb712a020a3631b9a52c4907d833b33513bc7e21
FedeClaudi <federicoclaudi@protonmail.com>
2020-02-01 09:48:15 +0000

    bump

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name="fcutils",
-    version="0.1.3.3",
+    name="behaviour",
+    version="0.0.1",
     author_email="federicoclaudi@protonmail.com",
-    description="bunch of utility functions",
+    description="bunch of utility functions to analyse behaviour data",
     packages=find_namespace_packages(exclude=()),
     include_package_data=True,
-    url="https://github.com/FedeClaudi/fcutils",
+    url="https://github.com/BrancoLab/Behaviour",
     author="Federico Claudi",
     zip_safe=False,
 )
