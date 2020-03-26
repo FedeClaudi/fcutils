dce0b1d0d2f76b1fbcafd71c52cfee52651c5aee
FedeClaudi <federicoclaudi@protonmail.com>
2020-03-26 14:26:07 +0000

    bump

--- fcutils/objects.py
+++ fcutils/objects.py
@@ -8,16 +8,23 @@ def sort_list_of_strings(lst):
         Sorts a list like ["1", "10", "2", "25"] correctly
     """
 
-    def atoi(text):
-        return int(text) if text.isdigit() else text
+
+    def atof(text):
+        try:
+            retval = float(text)
+        except ValueError:
+            retval = text
+        return retval
 
     def natural_keys(text):
         '''
         alist.sort(key=natural_keys) sorts in human order
         http://nedbatchelder.com/blog/200712/human_sorting.html
         (See Toothy's implementation in the comments)
+        float regex comes from https://stackoverflow.com/a/12643073/190597
         '''
-        return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+        return [ atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', text) ]
+
 
     return lst.sort(key=natural_keys)
 
