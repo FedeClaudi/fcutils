6c626afbf55feea6bed7a7f10d7238a8996b23a6
FedeClaudi <federicoclaudi@protonmail.com>
2020-04-13 13:17:46 +0100

    bump

--- fcutils/plotting/quick_plots.py
+++ fcutils/plotting/quick_plots.py
@@ -13,3 +13,11 @@ def quick_scatter(x, y, ax_kwargs={}, **kwargs):
     clean_axes(f)
     ax.set(**ax_kwargs)
     return  f, ax
+
+def quick_plot(*args, ax_kwargs={}, **kwargs):
+    f, ax = plt.subplots()
+    for var in args:
+        ax.plot(var, **kwargs)
+    clean_axes(f)
+    ax.set(**ax_kwargs)
+    return f, ax
