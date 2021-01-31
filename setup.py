91dc99d98a40cd2cad5f91cac4f4eb6a5e016bc6
Federico Claudi <federicoclaudi@protonmail.com>
2021-01-31 17:28:11 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name="fcutils",
-    version="0.1.4.5",
+    version="1.1",
     author_email="federicoclaudi@protonmail.com",
     description="bunch of utility functions",
     packages=find_namespace_packages(exclude=()),
